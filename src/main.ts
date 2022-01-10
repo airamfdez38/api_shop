0ed893b29842035b9a462182331cad852739fd34
Manu_Wolae <mfdez@wolae.com>
2022-01-10 22:52:11 +0100

    try

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const config = new DocumentBuilder()
-  .setTitle('Api Shop')
-  .setDescription("API to manage my shop")
-  .setVersion('1.0')
-  .build()
+    .setTitle('Api Shop')
+    .setDescription("API to manage my shop")
+    .setVersion('1.0')
+    .build()
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document)
-  await app.listen(process.env.PORT || 3000);
+  await app.listen((process.env.PORT, '0.0.0.0') || 3000);
 }
 bootstrap();
