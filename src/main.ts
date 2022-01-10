5e2aad5637da65935ed902d3099b3788c3380841
Manu_Wolae <mfdez@wolae.com>
2022-01-10 23:17:26 +0100

    ...

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
     .build()
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document)
-  await app.listen(process.env.PORT || 3002);
+  const port = process.env.PORT || 3000
+  await app.listen(port);
 }
 bootstrap();
